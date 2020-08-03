0650701ed55db57da74b2f089fe10c4fe8edfbf2
Blessing <blessingthinker@gmail.com>
2020-08-03 23:12:55 +0200

    init

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -9,7 +9,7 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   items;
   constructor(private http: HttpClient) {
-    this.http.get('https://todo-example.vercel.app/db.json').subscribe(result => {
+    this.http.get('https://todo-example-server.vercel.app/db.json').subscribe(result => {
       this.items = result;
     });
   }
